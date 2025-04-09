26e7665f1f85f1786253d7a8ccf535afded4a206
Katsukv <katsukv06@gmail.com>
2025-04-09 03:13:20 +0300

    Игнорировать cmake-build-debug

--- cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_double_game_FILE C:/D/proga/bsu/rkpp/double_game/cmake-build-debug/double_game.exe)
-set(__QT_DEPLOY_TARGET_double_game_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_double_game_RUNTIME_DLLS )
